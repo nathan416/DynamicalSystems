cee5aa378bce06c8d42e393f9b7353bb8db5a3a0
nathan416 <nathan.flack@gmail.com>
2022-05-20 13:09:25 -0700

    updated readme and optimized plot_julia_set

--- README.md
+++ README.md
@@ -1,4 +1,197 @@
-# DynamicalSystems
+Nathan Flack
+
+CPSC 455
+
+Final Project
+
+Complex Iterative Map
+
+![](./media/image1.png){width="6.5in" height="5.135416666666667in"}
+
+Consider the Julia set given in Figure (1). Take the mapping z~n~+1 =
+z^2^~n~ + c where c = −0.5 + 0.3i
+
+\(a\) Iterate the initial point z0 = 0 + 0i for 500 iterations and list
+the final 100. Increase the number of iterations. What can you say about
+the orbit?
+
+Final 100 iterations after 500 iterations:
+
+-0.38254595+0.16996282j
+
+-0.38254595+0.16996282j
+
+-0.38254595+0.16996282j
+
+...
+
+...
+
+...
+
+-0.38254595+0.16996282j
+
+-0.38254595+0.16996282j
+
+-0.38254595+0.16996282j
+
+Final 100 iterations after 5000 iterations:
+
+-0.38254595+0.16996282j
+
+-0.38254595+0.16996282j
+
+-0.38254595+0.16996282j
+
+...
+
+...
+
+...
+
+-0.38254595+0.16996282j
+
+-0.38254595+0.16996282j
+
+-0.38254595+0.16996282j
+
+The orbit is attracted to the fixed point at -0.38254595+0.16996282j
+
+\(b\) Iterate the initial point z0 = −1 − i and list z1 to z10. What can
+you deduce about this orbit?
+
+-1.0-1.0j,
+
+ -0.5+2.3j,
+
+ -5.54-2.0j,
+
+ 26.1916+22.46j,
+
+ 181.04831056+1176.826672j,
+
+ -1352143.02517396+426125.26157509j,
+
+ 1.64670802e+12-1.1523646e+12j,
+
+ 1.38370314e+24-3.79521606e+24j,
+
+ -1.24890306e+49-1.05029047e+49j,
+
+ 4.56648772e+97+2.62342198e+98j
+
+This orbit increases to infinity as it iterates.
+
+2\. Given c = −1 + 1, determine the fixed points of periods one and two.
+
+2a. fixed points
+
+z\^2 - 1 + 1i = z
+
+z~1~= -0.693897202308099 + 0.418796525390441i
+
+z~2~= 1.6938972023081 - 0.418796525390441i
+
+2b. period two
+
+(z\*\*2 - 1 + 1i)\*\*2 - 1 + 1i = z
+
+Z~3~ = -1.30024259022012 + 0.624810533843827i,
+
+Z~4~ = 0.30024259022012 - 0.624810533843827i,
+
+3\. Plot the Julia sets for
+
+\(a\) c = 0 + 0i,
+
+![](./media/image2.png){width="6.5in" height="6.5in"}
+
+\(b\) c = −0.5 + 0i,
+
+![](./media/image3.png){width="6.5in" height="6.5in"}
+
+\(c\) −0.7 + 0i,
+
+![](./media/image4.png){width="6.489583333333333in"
+height="4.333333333333333in"}
+
+\(d\) −2 + 0i.
+
+![](./media/image5.png){width="6.479166666666667in"
+height="2.4270833333333335in"}
+
+4\. Compute the fixed points of period one for
+$z_{n + 1} = 2 + \frac{ze^{i{|z|}^{2}}}{10}$
+
+$$2 + \frac{ze^{i{|z|}^{2}}}{10} = z$$
+
+Z=1.80619 - 0.0211285i
+
+5\. Determine the boundaries of points of periods one and two for the
+mapping $z_{n + 1} = c - \ z_{n}^{2}$
+
+$$c - \ z_{n}^{2} = z$$
+
+Period one:
+
+$$z = \frac{1}{2}( \pm \sqrt{4c + 1} - 1)$$
+
+Period two:
+
+$$z = \frac{1}{2}( \pm \sqrt{4c - 3} + 1)$$
+
+Additional plots: z~n~+1 = z^2^~n~ + c
+
+![](./media/image6.png){width="6.5in" height="6.5in"}
+
+$$z_{n + 1} = 2 + \frac{ze^{i{|z|}^{2}}}{10}$$
+
+![](./media/image7.png){width="6.489583333333333in"
+height="6.489583333333333in"}
+
+$$z_{n + 1} = c - z_{n}^{2}$$
+
+![](./media/image8.png){width="6.489583333333333in"
+height="6.489583333333333in"}
+
+z~n~+1 = z^2^~n~ + c
+
+![](./media/image9.png){width="6.5in" height="6.5in"}
+
+$$z_{n + 1} = z_{n}^{3} + c$$
+
+![](./media/image10.png){width="6.5in" height="6.5in"}
+
+$$z_{n + 1} = z_{n}^{4} + c$$
+
+![](./media/image11.png){width="6.5in" height="6.5in"}
+
+$$z_{n + 1} = z_{n}^{3} + c$$
+
+![](./media/image12.png){width="6.5in" height="6.5in"}
+
+$$z_{n + 1} = z_{n}^{6} + c$$
+
+![](./media/image13.png){width="6.489583333333333in"
+height="3.6458333333333335in"}
+
+![](./media/image14.png){width="6.489583333333333in"
+height="3.6458333333333335in"}
+
+$z_{n + 1} = \frac{1 - \frac{z_{n}^{3}}{6}}{{{(z}_{n -}\frac{z_{n}^{2}}{2})}^{2}}$![](./media/image15.png){width="6.489583333333333in"
+height="3.6458333333333335in"}
+
+![](./media/image16.png){width="6.489583333333333in"
+height="3.6458333333333335in"}
+
+$z_{n + 1} = 1 - z_{n}^{2} + \frac{z_{n}^{5}}{2 + 4z_{n}}$ + c
+
+![](./media/image17.png){width="6.489583333333333in"
+height="3.6458333333333335in"}![](./media/image18.png){width="6.489583333333333in"
+height="3.6458333333333335in"}
+
+$$z_{n + 1} = 1 - z_{n}^{3} + \frac{z_{n}^{6}}{2 + 4z_{n}} + 0.7885\ e^{ia}\ \text{for\ 0} < a\  < 2\pi$$
+
 ## plot_julia_set
 ![img](https://i.imgur.com/kxPxZgB.gif)
 ![img](https://i.imgur.com/61PyTUJ.gif)
--- julia_set_image.py
+++ julia_set_image.py
@@ -33,39 +33,62 @@ from matplotlib import cm
 
 from dynamical import *
 
-REAL_RANGE_MIN = -2.31
-REAL_RANGE_MAX = 2.31
+REAL_RANGE_MIN = -1.0
+REAL_RANGE_MAX = 1.0
 IMAG_RANGE_MIN = -1.3
 IMAG_RANGE_MAX = 1.3
 
 IMAGE_WIDTH = 1920
-IMAGE_HEIGHT = (IMAG_RANGE_MAX - IMAG_RANGE_MIN)*(IMAGE_WIDTH)/(REAL_RANGE_MAX - REAL_RANGE_MIN)
+IMAGE_HEIGHT = (IMAG_RANGE_MAX - IMAG_RANGE_MIN) * (IMAGE_WIDTH) / (REAL_RANGE_MAX - REAL_RANGE_MIN)
 
-FRAMES = 30
-DURATION = 150
-ITERATIONS = 400
+FRAMES = 2000
+DURATION = 40
+ITERATIONS = 110
+
+FILENAME = f'pictures/over_time3/overtime12.gif'
 
 LOGGER = logging.getLogger(__name__)
 LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -43s %(funcName) '
               '-35s %(lineno) -5d: %(message)s')
 
-norm = matplotlib.colors.Normalize(vmin=0, vmax=399)
-# @cuda.jit('void(complex64[:,:], float32)')
-# def complex_function(x, a):
-#     # i = cuda.grid(1)
-#     # x[i] = x[i]**2 - 0.4 + 0.6j
-#     # start = cuda.grid(1)
-#     # stride = cuda.gridsize(1)
-#     xstart, ystart = cuda.grid(2)
-#     xstride, ystride = cuda.gridsize(2)
-#     for i in range(xstart, x.shape[0], xstride):
-#         for j in range(ystart, x.shape[1], ystride):
-#             # x[i, j] = 1 - x[i, j]**2 + x[i, j]**2 / (2 + 4 * x[i, j]) + 0.7885 * np.e**(a * 1j)
-#             # x[i, j] = 1 - x[i, j] + x[i, j]**2 + 0.7885 * np.e**(a * 1j)
-#             x[i, j] = x[i, j]**4 + x[i, j]**3/(x[i, j]-1) + x[i, j]**2/(x[i, j]**3 + 4 *x[i, j]**2 + 5) - 0.5885 * np.e**(a * 1j)
+norm = matplotlib.colors.Normalize(vmin=0, vmax=110)
+
+# examples
+# x = 1 - x**2 + x**2 / (2 + 4 * x) + 0.7885 * np.e**(a * 1j)
+# x = 1 - x + x**2 + 0.7885 * np.e**(a * 1j)
+# x = x**4 + x**3/(x-1) + x**2/(x**3 + 4 *x**2 + 5) - 0.5885 * np.e**(a * 1j)
+# x = x**4 + x**3/(x-1) + x**2/(x**3 + 4 *x**2 + 5) + 0.755534*math.cos(a) + 0.737292*1j*math.cos(a) - 2*0.737292*1j
+# x = 2**x + 0.2885 * np.e**(a * 1j)
+# x = x**2 + 0.355534*math.cos(2*a)-0.337292*1j*math.cos(a)
+# x**4 + x**3 / (x - 1) + x**2 / (x**3 + 4 * x**2 + 5) + 0.377767 * math.sin(a) + 0.368646 * 1j * math.sin(a) - 0.368646 * 1j + 0.377767
+# x**2 + a*.01 - a*.3*1j
+
+
+@cuda.jit('void(complex64, float32)', device=True)
+def iterating_function(x, a):
+    return x**4 + x**3 / (x - 1) + x**2 / (x**3 + 4 * x**2 + 5) + 0.377767 * math.sin(a) + 0.368646 * 1j * math.sin(a) - 0.368646 * 1j + 0.377767
+
+
+@cuda.jit('void(complex64, float32)', device=True)
+def divergence_tracker(x, divergence):
+    if x.real**2 + x.imag**2 < 300:
+        return divergence + 1
+    return divergence
+
+
+@cuda.jit('void(complex64[:,:], int32[:,:], float32, int32)')
+def helper_func5_image(x, divergence, a, iter):
+    xstart, ystart = cuda.grid(2)
+    xstride, ystride = cuda.gridsize(2)
+    for k in range(iter):
+        for i in range(xstart, x.shape[0], xstride):
+            for j in range(ystart, x.shape[1], ystride):
+                x[i, j] = iterating_function(x[i, j], a)
+                divergence[i, j] = divergence_tracker(x[i, j], divergence[i, j])
+
 
 def plot_julia_set_image(expression: callable, a, iteration_count: int = 100, real_range_min: float = -1.0, real_range_max: float = 1.0,
-                   imag_range_min: float = -1.0, imag_range_max: float = 1.0, cmap='viridis', image_width=1920, image_height=1080, fig_name='Julia set plot'):
+                         imag_range_min: float = -1.0, imag_range_max: float = 1.0, cmap='viridis', image_width=1920, image_height=1080, fig_name='Julia set plot'):
     """ takes complex expression, iterates the expression with a random set of initial complex points
         and returns the times it was iterated before the value diverged.
         removes values that did not diverge. plots the remaining points on a complex plane
@@ -83,17 +106,18 @@ def plot_julia_set_image(expression: callable, a, iteration_count: int = 100, re
 
     """
     start_time = time.perf_counter()
-    
+
     real_set = np.linspace(real_range_min, real_range_max, int(image_width)).reshape((1, int(image_width)))
     imag_set = np.linspace(imag_range_max, imag_range_min, int(image_height)).reshape((int(image_height), 1))
     complex_set = np.array(real_set + 1j * imag_set, dtype=np.complex64)
-    
-    divergence = iterations_till_divergence_image(expression, np.array(complex_set, dtype=np.complex64), a, iteration_count)
-        
-    LOGGER.info(f'time taken plot_julia_set: {time.perf_counter() - start_time}')
-    return complex_set, divergence
 
-def iterations_till_divergence_image(expression: callable, initial_values: np.ndarray, a:float, iteration_count=1000) -> list:
+    divergence = iterations_till_divergence_image(expression, np.array(complex_set, dtype=np.complex64), a, iteration_count)
+
+    LOGGER.info(f'time taken plot_julia_set: {time.perf_counter() - start_time}')
+    return divergence
+
+
+def iterations_till_divergence_image(expression: callable, initial_values: np.ndarray, a: float, iteration_count=1000) -> list:
     """ iterates over the initial values with a function and returns
         a list of when the modulus of each complex value goes above 4
         array can be multidimensional but the function must change to account
@@ -111,52 +135,46 @@ def iterations_till_divergence_image(expression: callable, initial_values: np.nd
     iterating_values_h = initial_values
     divergence_h = np.zeros(initial_values.shape, dtype=np.int32)
 
-    griddim = 64
-    blockdim = 32
-    
+    blockdim = (16, 16)
+    griddimx = math.ceil(IMAGE_WIDTH / blockdim[0])
+    griddimy = math.ceil(IMAGE_HEIGHT / blockdim[1])
+    griddim = (griddimx, griddimy)
+
     iterating_values_d = cuda.to_device(iterating_values_h)
     divergence_d = cuda.to_device(divergence_h)
-    # for iteration in range(iteration_count):
-        # expression[griddim, blockdim](iterating_values_d, a)
+    
     expression[griddim, blockdim](iterating_values_d, divergence_d, a, iteration_count)
+    
     divergence_h = divergence_d.copy_to_host()
     LOGGER.info(f'time taken iterations_till_divergence_image: {time.perf_counter() - start_time}')
     return divergence_h
 
-@cuda.jit('void(complex64[:,:], int32[:,:], float32, int32)')
-def helper_func5_image(x, divergence, a, iter):
-    xstart, ystart= cuda.grid(2)
-    xstride, ystride = cuda.gridsize(2)
-    for k in range(iter):
-        for i in range(xstart, x.shape[0], xstride): 
-            for j in range(ystart, x.shape[1], ystride):
-                # x[i, j] = x[i, j]**4 + x[i, j]**3/(x[i, j]-1) + x[i, j]**2/(x[i, j]**3 + 4 *x[i, j]**2 + 5) + 0.355534*math.cos(2*a)-0.337292*1j*math.cos(a)
-                # x[i, j] = 2**x[i, j] + 0.2885 * np.e**(a * 1j)
-                x[i, j] = x[i, j]**2 + 0.355534*math.cos(2*a)-0.337292*1j*math.cos(a)
-                if x[i,j].real**2 + x[i,j].imag**2 < 4:
-                    divergence[i,j] = divergence[i,j] + 1
 
-def plotting_function(frames):
+def plotting_function():
     start_time = time.perf_counter()
 
     # for a in tqdm(np.linspace(0, 2*np.pi, 10)):
     #     cleaned_list, cleaned_divergence = plot_julia_set_image(helper_func5_image, a, 110, REAL_RANGE_MIN, REAL_RANGE_MAX, IMAG_RANGE_MIN, IMAG_RANGE_MAX, 'RdYlGn', 19.2, 10.8)
     #     fig_list.append(cleaned_divergence)
-    imgs = process_map(plotting_helper, np.linspace(-.1, .1, frames), chunksize=6, max_workers=psutil.cpu_count(False))
+    # imgs = process_map(plotting_helper, np.linspace(0, 2*np.pi, FRAMES), chunksize=1, max_workers=psutil.cpu_count(False))
+    imgs = []
+    for a in tqdm(np.linspace(0, 2*np.pi, FRAMES)):
+        imgs.append(plotting_helper(a))
+        
     LOGGER.info(f'size of imgs {asizeof.asizeof(imgs)}')
     LOGGER.info(f'time taken plotting_function: {time.perf_counter() - start_time}')
     return imgs
-    
+
+
 def plotting_helper(a):
     # norm = (cleaned_divergence - np.min(cleaned_divergence)) / (np.max(cleaned_divergence) - np.min(cleaned_divergence))
-    cleaned_list, cleaned_divergence = plot_julia_set_image(helper_func5_image, a, ITERATIONS, REAL_RANGE_MIN, REAL_RANGE_MAX, IMAG_RANGE_MIN, IMAG_RANGE_MAX, 'RdYlGn', IMAGE_WIDTH, IMAGE_HEIGHT)
-    
-    normalized = norm(cleaned_divergence)
+    divergence = plot_julia_set_image(helper_func5_image, a, ITERATIONS, REAL_RANGE_MIN, REAL_RANGE_MAX, IMAG_RANGE_MIN, IMAG_RANGE_MAX, 'RdYlGn', IMAGE_WIDTH, IMAGE_HEIGHT)
+    normalized = norm(divergence)
 
-    im = Image.fromarray(np.uint8(cm.gnuplot2(normalized)*255))
+    im = Image.fromarray(np.uint8(cm.gnuplot(normalized) * 255))
     I1 = ImageDraw.Draw(im)
-    my_font = ImageFont.truetype('arial', 15)
-    I1.text((28, 36), f'x**2 + {0.355534*math.cos(2*a)-0.337292*1j*math.cos(a)}', fill=(255, 0, 0), font=my_font)
+    my_font = ImageFont.truetype('arial', 20)
+    I1.text((28, 36), f'x = x**4 + x**3/(x-1) + x**2/(x**3 + 4 *x**2 + 5) + {0.377767 * math.sin(a) + 0.368646 * 1j * math.sin(a) - 0.368646 * 1j + 0.377767}', fill=(255, 0, 0), font=my_font)
     # im.save(f'pictures/over_time3/{int(a*100000)}.png')
     # plt.imsave(f'pictures/over_time3/{int(a*100000)}.png', cleaned_divergence, cmap='twilight_shifted')
     return im
@@ -167,11 +185,17 @@ def main():
     logging.getLogger('matplotlib.font_manager').setLevel(logging.WARNING)
     logging.getLogger('numba.core').setLevel(logging.WARNING)
     logging.getLogger('numba.cuda.cudadrv.driver').setLevel(logging.WARNING)
-    imgs = plotting_function(FRAMES)
+    imgs = plotting_function()
     img = imgs[0]  # extract first image from iterator
-    img.save(fp=f'pictures/over_time3/overtime9.gif', format='GIF', append_images=imgs[1:],
-         save_all=True, duration=DURATION, loop=0)
-    
+    img.save(fp=FILENAME, format='GIF', append_images=imgs[1:],
+             save_all=True, duration=DURATION, loop=0)
+
+
 if __name__ == '__main__':
     multiprocessing.freeze_support()
     main()
+
+
+
+    
+    
